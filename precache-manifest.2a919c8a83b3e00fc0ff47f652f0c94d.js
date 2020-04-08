86eb68f1f41dbd80111193268b6f0ddc25678666
Scott Wilkins <scottwilkins@Scotts-MacBook-Pro.local>
2020-04-08 16:03:16 -0700

    Updates

---
+++ precache-manifest.2a919c8a83b3e00fc0ff47f652f0c94d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4048fbf17b054a694db72727504751f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d70fb5c66e15502619e",
+    "url": "/monsters-rolodex/static/css/main.9686cdeb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d70fb5c66e15502619e",
+    "url": "/monsters-rolodex/static/js/main.9ab3e10e.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
